c63c9fc351842e8d5753896359604b64b0900f9f
Ellis Zhao <zhaotieqiang@gmail.com>
2016-06-18 15:01:29 +0800

    解决冲突

--- src/main/java/weixin/popular/api/DeviceAPI.java
+++ src/main/java/weixin/popular/api/DeviceAPI.java
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 package weixin.popular.api;
 
 /**
@@ -6,7 +7,7 @@ package weixin.popular.api;
  * @author nobody
  *
  */
-public class DeviceAPI extends BaseAPI{
+public class DeviceAPI extends BaseAPI {
 
 	/**
 	 * 这里一片空白，开源贡献动起手来！
@@ -15,21 +16,4 @@ public class DeviceAPI extends BaseAPI{
 	 * 
 	 */
 }
-=======
-package weixin.popular.api;
 
-/**
- * 设备功能
- * @author nobody
- *
- */
-public class DeviceAPI extends BaseAPI{
-
-	/**
-	 * 这里一片空白，开源贡献动起手来！
-	 * 
-	 * 参考其它API的开发方式
-	 * 
-	 */
-}
->>>>>>> 0727dc61a0ebb79e9b3391976937a54697002b5f
